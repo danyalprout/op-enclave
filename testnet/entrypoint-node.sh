7a35a82dfb459db7c4aedd25115de6cc5636156a
Danyal Prout <me@dany.al>
2024-10-27 14:03:50 -0500

    Remove jwt overrides

--- testnet/entrypoint-node.sh
+++ testnet/entrypoint-node.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-echo "$L2_ENGINE_JWT" > /tmp/engine.jwt
-
 if [[ $POD_NAME == *-0 ]]; then
   echo "Leader pod"
   export OP_NODE_SEQUENCER_ENABLED=true
 fi
 
-exec ./op-node \
-  --l2.jwt-secret=/tmp/engine.jwt
+exec ./op-node
